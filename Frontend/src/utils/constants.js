fb0d729e4ddeb1fdd15b3335f8873fcf039dd891
VaddeSurya <22a91a12c0@aec.edu.in>
2025-04-12 21:41:27 +0530

    third

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const URL = "https://budgetmaker.onrender.com"
-export const URL = "http://localhost:3001"
+export const URL = "https://budgetmaker.onrender.com"
+// export const URL = "http://localhost:3001"
